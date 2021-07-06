b3ff37e8e001e497c8dc516278523cacac67e71b
Klaus Buecher <buecher@optosolar.com>
2021-07-06 15:24:20 +0200

    changes by John

--- background.js
+++ background.js
@@ -1,173 +1,342 @@
-/*
-* License:  see License.txt
+/**
+ * License: see License.txt
+ *
+ * Code  for TB 68 or later: Creative Commons (CC BY-ND 4.0):
+ *      Attribution-NoDerivatives 4.0 International (CC BY-ND 4.0) 
+ *
+ * Copyright: Klaus Buecher/opto 2021
+ * Contributors: see Changes.txt
+ */
 
-* Code  for TB 68 or later: Creative Commons (CC BY-ND 4.0):
-*      Attribution-NoDerivatives 4.0 International (CC BY-ND 4.0) 
- 
-* Copyright: Klaus Buecher/opto 2021
-* Contributors:  see Changes.txt
-*/
-
-
-
-/*
-
-
-
-Attribution:
-icon by <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-
-//versioncompare   from https://jsfiddle.net/vanowm/p7uvtbor/
+/**
+ * Attribution:
+ * - icon by <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+ * - versioncompare from https://jsfiddle.net/vanowm/p7uvtbor/
+ */
 
 
-*/
+// Enforce database rebuild every 24h.
+const rebuildInterval = 24*60*60*1000;
 
+// Enable debug mode in production (it is enabled automatically in add-on debug install mode)
+var debug = false;
 
-
-// landing windows.
-messenger.runtime.onInstalled.addListener(async ({ reason, temporary }) => {
-	// if (temporary) return; // skip during development
-	switch (reason) {
-		case "install":
-			{
-				const url = messenger.runtime.getURL("popup/installed.html");
-				//await browser.tabs.create({ url });
-//				await messenger.windows.create({ url, type: "popup", height: 780, width: 1190, });
-			}
-			break;
-		case "update":
-			{
-				const url = messenger.runtime.getURL("popup/update.html");
-				//await browser.tabs.create({ url });
-//				await messenger.windows.create({ url, type: "popup", height: 780, width: 1190, });
-			}
-			break;
-		// see below
-	}
-});
-/**/
-
-window.setInterval(checkAddons, 24*60*1000);
-
-function openOptions() {
-//	console.log("open options");
-	messenger.tabs.create({url:"/popup/tt/ttt/options.html"});
-//	messenger.runtime.openOptionsPage();
+// A simple log wrapper.
+function log(...args) {
+	if (debug) console.log(...args);
 }
-messenger.browserAction.onClicked.addListener(openOptions);
 
-async function checkAddons() {
-	//console.log("running checkAddons");
-	let iOK = 0, iInCompatible = 0, iDisabledIncompatible = 0, sIncompatibleNames = [], sDisabledIncompatibleNames = [];
-	messenger.browserAction.setBadgeText({ text: "…" });
-	messenger.browserAction.setBadgeBackgroundColor({ color: "blue" });
+// Common check on compatibility string.
+function isCompatible(value) {
+	return (
+		value == "*" ||
+		parseInt(value.split(".").shift()) >= 91
+	);
+}
 
-	let addons = await messenger.management.getAll();
-	//console.log("raw addons", addons);
-	let results = [];
-	for (let addon of addons) {
-		if (
-			addon.installType != "normal" ||
-			addon.type != "extension" ||
-			addon.id.endsWith("mozilla.org")
-		) {
-			continue;
+// Open the tab with the detailed compatibility information.
+function openCompatibilityView() {
+	messenger.tabs.create({url:"/popup/tt/ttt/options.html"});
+}
+
+// A versioncompare, taken from https://jsfiddle.net/vanowm/p7uvtbor/
+function compareVer(a, b) {
+	function prep(t) {
+	  return ("" + t)
+		//treat non-numerical characters as lower version
+		//replacing them with a negative number based on charcode of first character
+		.replace(/[^0-9\.]+/g, function (c) { return "." + ((c = c.replace(/[\W_]+/, "")) ? c.toLowerCase().charCodeAt(0) - 65536 : "") + "." })
+		//remove trailing "." and "0" if followed by non-numerical characters (1.0.0b);
+		.replace(/(?:\.0+)*(\.-[0-9]+)(\.[0-9]+)?\.*$/g, "$1$2")
+		.split('.');
+	}
+	a = prep(a);
+	b = prep(b);
+	for (var i = 0; i < Math.max(a.length, b.length); i++) {
+	  //convert to integer the most efficient way
+	  a[i] = ~~a[i];
+	  b[i] = ~~b[i];
+	  if (a[i] > b[i])
+		return 1;
+	  else if (a[i] < b[i])
+		return -1;
+	}
+	return 0;
+}
+
+async function getInstalledExtensions() {
+	let results = await messenger.management.getAll();
+	return results.filter(addon => 
+			addon.installType == "normal" &&
+		  	addon.type == "extension" &&
+		  	!addon.id.endsWith("mozilla.org")
+	);
+}
+
+// Convert YAML to JSON.
+async function dataToJSON(data) {
+	let entries = [];
+  
+	let lines = data.split(/\r\n|\n/);
+	let i = 0;
+  
+	do {
+	  let entry = {};
+	  while (i < lines.length) {
+		i++;
+		let line = lines[i - 1].trim();
+  
+		// End of Block
+		if (line.startsWith("---")) {
+		  break;
 		}
-
-		let rv = await fetch(`https://addons.thunderbird.net/api/v4/addons/addon/${addon.id}/versions/`);
-		let response = await rv.json();
-		//	console.log("raw response", response);
-		var comps = [];
-		if (
-			Array.isArray(response.results) &&
-			response.results.length > 0
-		) {
-			for (let result of response.results) {
-				comps.push(result.compatibility.thunderbird.max);
-
-
-			}
-			//	console.log("comps",comps);
-			let data = response.results[0];
-			//		console.log(response.results[0].url);
-			let info = {
-				name: addon.name,
-				version: data.version,
-				compatibility: data.compatibility.thunderbird.max,
-				enabled: addon.enabled,
-			};
-			results.push(info);
+		// Skip comments.
+		if (line.startsWith("#")) {
+		  continue;
 		}
+		let parts = line.split(":");
+		let key = parts.shift().trim();
+		if (key) {
+		  let value = parts.join(":").trim();
+		  entry[key] = value;
+		}
+	  }
+  
+	  // Add found entry.
+	  if (Object.keys(entry).length > 0) {
+		entries.push(entry);
+	  }
+	} while (i < lines.length);
+  
+	return entries;
+}
+
+// Get alternative database.
+async function queryAlternativeData() {
+	let url = "https://raw.githubusercontent.com/thundernest/extension-finder/master/data.yaml"
+	return fetch(url).then(r => r.text()).then(dataToJSON);
+}
+
+// Get add-on data from ATN.
+async function queryAddonData(addon, alternativeData) {
+	let info = {
+		id: addon.id,
+		name: addon.name,
+		enabled: addon.enabled,
+		version: addon.version,
+		description: addon.description,
+		alternative: "",
+		maxVersion: "unknown", // highest version available on ATN
+		maxCompatibility: "unknown", // compatibility of highest version on ATN
+		maxUrl: "unknown", // url of highest version on ATN
+	};
+
+	// Check alternative info.
+	let addonAltern = alternativeData.filter(o => o.u_id === addon.id);
+	if (addonAltern.length) {
+		info.alternative = "<a href = 'https://extension-finder.thunderbird.net/?id=" + addon.id + "' id = 'altaddon' >more info</a>";
 	}
 
-	// TODO: open tab and list state for each version
-	//	console.log(results);
-
-
-	results.forEach((info) => {
-		//	console.log("comp", info.compatibility, info.name);
-		if (info.compatibility == "*" ||
-			parseInt(info.compatibility.split(".").shift()) >= 91) {
-			//		messenger.browserAction.setBadgeText({text:"✓"});
-			//		messenger.browserAction.setBadgeBackgroundColor({color:"green"});
-			//		if (info.enabled)   
-			iOK++;
-		} else {
-			if (info.enabled) {
-				iInCompatible++;
-				sIncompatibleNames.push(info.name);
-			} else {
-				sDisabledIncompatibleNames.push(info.name);
-				iDisabledIncompatible++;
-			}
-			//
-			//		messenger.browserAction.setBadgeText({text:iInCompatible+"✗"});
-			//		messenger.browserAction.setBadgeBackgroundColor({color:"red"});
+	// Get info from ATN.
+	let rv = await fetch(`https://addons.thunderbird.net/api/v4/addons/addon/${addon.id}/versions/`);
+	let response = await rv.json();
+	if (
+	  Array.isArray(response.results) &&
+	  response.results.length > 0
+	) {
+		// Collect information from returned add-ons on ATN.
+		let compVers = [];
+		for (let result of response.results) {
+			compVers.push({
+				version: result.version,
+				compatibility: result.compatibility.thunderbird.max,
+				url: result.url,
+			});
 		};
-	});
-	//	console.log("ok, en not en", iOK, iInCompatible, iDisabledIncompatible);
-	if ((iInCompatible == 0) && (iDisabledIncompatible == 0)) {
-		messenger.browserAction.setBadgeText({ text: iOK + "✓" });
-		messenger.browserAction.setBadgeBackgroundColor({ color: "green" });
-
+		
+		// Find max version and take its compat info.
+		compVers.sort((a, b) => { compareVer(a.version, b.version) });
+		info.maxVersion = compVers[0].version;
+		info.maxCompatibility = compVers[0].compatibility;
+		info.maxUrl = `<a href="${compVers[0].url}">${compVers[0].version}</a>`;
 	}
-	else {
-		messenger.browserAction.setBadgeText({ text: iInCompatible + "/" + iDisabledIncompatible });
+
+	// Calculate compatibility.
+	if (isCompatible(info.maxCompatibility)) {
+		info.status = "isOk";
+	} else if (info.maxCompatibility == "unknown") {
+		info.status = "isUnknown";
+	} else if (info.alternative) {
+		info.status = "hasAlternative";
+	} else {
+		info.status = "isIncompatible";
+	}
+
+	return info;
+}
+
+// Check if the local copy of the addonData and alternativeData needs to be updated.
+// This function is gaiting itself to ensure rate limits on the external servers.
+async function checkAddons(action = "updateAddonData", info = null) {
+	let { alternativeData } = await messenger.storage.local.get("alternativeData");
+	let { addonData } = await messenger.storage.local.get("addonData");
+	let { lastCheck } = await messenger.storage.local.get("lastCheck");
+
+	// Enforce rebuild, if no database or outdated database.
+	if (
+		!alternativeData || 
+		!addonData ||
+		!lastCheck ||
+		lastCheck < Date.now() - rebuildInterval
+	) {
+		action = "rebuild";
+	}
+
+	if (action == "rebuild") {
+		log("Rebuilding databases ...");
+		messenger.browserAction.setBadgeText({ text: "…" });
+		messenger.browserAction.setBadgeBackgroundColor({ color: "blue" });
+		messenger.browserAction.disable();
+
+		alternativeData = await queryAlternativeData();
+		await messenger.storage.local.set({ alternativeData });
+
+		addonData = {};
+		action = "updateAddonData";
+		await messenger.storage.local.set({ lastCheck: Date.now() });
+	}
+	
+	if (action == "updateAddonData") {
+		log("Check if database is missing any installed add-ons ...");
+		let installedAddons = await getInstalledExtensions();
+		let missingAddons = installedAddons.filter(addon => !addonData.hasOwnProperty(addon.id));
+		log({ missingAddons });
+		// Add any missing add-ons to the database.
+		for (let missingAddon of missingAddons) {
+			addonData[missingAddon.id] = await queryAddonData(missingAddon, alternativeData);
+		}
+
+		log("Check if database needs update, because add-on(s) have been removed/disabled/enabled ...");
+		let removedAddons = Object.values(addonData).filter(addon => !installedAddons.some(a => a.id == addon.id));
+		log({ removedAddons });
+		// Remove any no longer installed add-ons from the database.
+		for (let removedAddon of removedAddons) {
+			delete addonData[removedAddon.id];
+		}
+		// Update the enable/disable states of all remaining add-ons.
+		for (let addon of installedAddons) {
+			addonData[addon.id].enabled = addon.enabled;
+		}
+		await messenger.storage.local.set({ addonData });
+	}
+	
+	if (info) {
+		log("Adjusting database to local add-on changes ...");
+		switch (action) {
+			case "enabled":
+			case "disabled":
+				if (addonData.hasOwnProperty(info.id)) {
+					addonData[info.id].enabled = info.enabled;
+					break;
+				}
+			case "installed":
+				addonData[info.id] = await queryAddonData(info, alternativeData);
+				break;
+			case "uninstalled":
+				delete addonData[info.id];
+				break;
+			}
+		await messenger.storage.local.set({ addonData });
+	}
+
+	log({alternativeData});
+	log({addonData});
+
+	// Update the browser action button.
+	let isOk = 0;
+	let isIncompatible = 0;
+	let isUnknown = 0;
+	let hasAlternative = 0;
+
+	for (let id in addonData) {
+		let info = addonData[id];
+		if (info.enabled) {			
+			log("enabled", {info});
+			if (info.status == "isOk") {
+				isOk++;
+			} else if (info.status == "isUnknown") {
+				isUnknown++;
+			} else if (info.status == "isIncompatible") {
+				isIncompatible++;
+			} else if (info.status == "hasAlternative") {
+				hasAlternative++;
+			}
+		}
+	};
+	let total = isOk + isIncompatible + hasAlternative + isUnknown;
+	log("results", {total, isOk, isIncompatible, hasAlternative, isUnknown});
+
+	if (isOk == total) {
+		// Perfect.
+		messenger.browserAction.setBadgeText({ text: `✓` });
+		messenger.browserAction.setBadgeBackgroundColor({ color: "green" });
+	} else if (isIncompatible == 0 && hasAlternative == 0 && isUnknown > 0) {
+		// What we know is perfect, but some are unknown.
+		messenger.browserAction.setBadgeText({ text: `-${isUnknown}` });
+		messenger.browserAction.setBadgeBackgroundColor({ color: "green" });
+	} else if (isIncompatible == 0 && hasAlternative > 0 && isUnknown == 0) {
+		// Not perfect, but we have a potential replacement for all add-ons.
+		messenger.browserAction.setBadgeText({ text: `✓` });
+		messenger.browserAction.setBadgeBackgroundColor({ color: "orange" });
+	} else if (isIncompatible == 0 && hasAlternative > 0  && isUnknown > 0) {
+		// Not perfect, but we have a potential replacement for all add-ons, but
+		// some are unknown.
+		messenger.browserAction.setBadgeText({ text: `-${isUnknown}` });
+		messenger.browserAction.setBadgeBackgroundColor({ color: "orange" });
+	} else {
+		// Bad, we have incompatible add-ons without replacements.
+		messenger.browserAction.setBadgeText({ text: `-${isIncompatible + isUnknown}` });
 		messenger.browserAction.setBadgeBackgroundColor({ color: "red" });
 	}
-	let resTitle = "Check add-on updates and compatibility in next ESR" + "\n\n"
-	resTitle += "Active addons, incompatible: " + sIncompatibleNames.toString() + "\n\n";
-	resTitle = resTitle + "Deactivated addons, incompatible: " + sDisabledIncompatibleNames +
-		"\n\nMore info at Tools->Addons / option page of this addon";
-	//	console.log("nmes", sIncompatibleNames, sDisabledIncompatibleNames);
-//	messenger.browserAction.setTitle({ title: resTitle });
-//
-
-
-
-
-/*
-	var addonNotification = "addon-notification"
-	messenger.notifications.create(addonNotification, {
-		"type": "basic",
-		"iconUrl": browser.runtime.getURL("/skin/puzzle-piece32.png"),
-		"title": "Addon compatibility",
-		"message": "More info at Tools->Addons / option page of this addon"
-
-	});
-
-*/
-	;
-
-
-
-
+	messenger.browserAction.enable();
+	
+	// Notify open view to update.
+	try {
+		await browser.runtime.sendMessage({command: "reloadView"});
+	} catch(e) {		
+		// Suppress warning about receiving end missing, if no view open.
+	} 
 }
 
-messenger.browserAction.onClicked.addListener(checkAddons);
+// Main.
+async function main() {
+	// Hide browserAction label if supported.
+	if (browser.browserAction.setLabel) {
+		browser.browserAction.setLabel({ label: "" });
+	}
+	
+  	// Enable debug mode on debug installs.
+	let self = await messenger.management.getSelf();
+	if (self.installType == "development") {
+		debug = true;
+	}
 
+	window.setInterval(() => checkAddons("rebuild"), rebuildInterval);
+	checkAddons();
 
+	messenger.browserAction.onClicked.addListener((tab) => {
+		// On TB68, tab is not available.
+		if (!tab || messenger.browserAction.isEnabled({tabId: tab.id})) {
+			openCompatibilityView();
+		}
+	});
+		
+	// The following need to update the button and the local information, but
+	// no need to query remote servers.
+	messenger.management.onInstalled.addListener((info) => checkAddons("installed", info));
+	messenger.management.onUninstalled.addListener((info) => checkAddons("uninstalled", info));
+	messenger.management.onEnabled.addListener((info) => checkAddons("enabled", info));
+	messenger.management.onDisabled.addListener((info) => checkAddons("disabled", info));
+}
 
-
-
-window.addEventListener("load", checkAddons);
+window.addEventListener("load", main);
--- popup/tt/ttt/options.js
+++ popup/tt/ttt/options.js
@@ -1,295 +1,118 @@
-/*
- * License:  see License.txt
-
+/**
+ * License: see License.txt
+ *
  * Code  for TB 68 or later: Creative Commons (CC BY-ND 4.0):
  *      Attribution-NoDerivatives 4.0 International (CC BY-ND 4.0) 
- 
+ *
  * Copyright: Klaus Buecher/opto 2021
- * Contributors:  see Changes.txt
+ * Contributors: see Changes.txt
  */
 
-
-var bgrPage = null;
-
-
-
-var addons = [];;
-
-
-
-
-async function loadData() {
-  let url = "https://raw.githubusercontent.com/thundernest/extension-finder/master/data.yaml"
-  return fetch(url).then(r => r.text()).then(dataToJSON);
+ // Enable debug mode in production (it is enabled automatically in add-on debug install mode)
+var debug = false;
+ 
+// A simple log wrapper.
+function log(...args) {
+  if (debug) console.log(...args);
 }
-
-async function dataToJSON(data) {
-  let entries = [];
-
-  let lines = data.split(/\r\n|\n/);
-  let i = 0;
-
-  do {
-    let entry = {};
-    while (i < lines.length) {
-      i++;
-      let line = lines[i - 1].trim();
-
-      // End of Block
-      if (line.startsWith("---")) {
-        break;
-      }
-      // Skip comments.
-      if (line.startsWith("#")) {
-        continue;
-      }
-      let parts = line.split(":");
-      let key = parts.shift().trim();
-      if (key) {
-        let value = parts.join(":").trim();
-        entry[key] = value;
-      }
-    }
-
-    // Add found entry.
-    if (Object.keys(entry).length > 0) {
-      entries.push(entry);
-    }
-  } while (i < lines.length);
-
-  return entries;
-}
-
-
-
 
 async function printAddons(event) {
-  // console.log("print");// 
   window.print();
-};
-
-
-
-
-
-//versioncompare
-
-
-//from https://jsfiddle.net/vanowm/p7uvtbor/
-function compareVer(a, b) {
-  function prep(t) {
-    return ("" + t)
-      //treat non-numerical characters as lower version
-      //replacing them with a negative number based on charcode of first character
-      .replace(/[^0-9\.]+/g, function (c) { return "." + ((c = c.replace(/[\W_]+/, "")) ? c.toLowerCase().charCodeAt(0) - 65536 : "") + "." })
-      //remove trailing "." and "0" if followed by non-numerical characters (1.0.0b);
-      .replace(/(?:\.0+)*(\.-[0-9]+)(\.[0-9]+)?\.*$/g, "$1$2")
-      .split('.');
-  }
-  a = prep(a);
-  b = prep(b);
-  for (var i = 0; i < Math.max(a.length, b.length); i++) {
-    //convert to integer the most efficient way
-    a[i] = ~~a[i];
-    b[i] = ~~b[i];
-    if (a[i] > b[i])
-      return 1;
-    else if (a[i] < b[i])
-      return -1;
-  }
-  return 0;
 }
-/* */
-
-
+ 
 function showOptoAddons() {
-
   messenger.tabs.create({ url: "otheraddons.html" });
-  //  messenger.windows.create({url: "otheraddons.html",  height: 600,  width: 890,  type: "popup"});
-
 }
-
+ 
 async function onLoad(event) {
-  // console.log("loaded");
-  // console.log("versions","1.10.1b1", "1.10.1b2", compareVer("1.10.1b1", "1.10.1b2"));
-  //addons
-  let results = await messenger.management.getAll();
-  let alternatives = await loadData();
-  // console.log("raw addons", addons);
-
-
-  var cBtn = document.getElementById("otherAddons");
-  cBtn.addEventListener("click", showOptoAddons, true);
-  //     console.log(clipBtn);
-
-
-
-  for (let addon of results) {
-    if (
-      addon.installType != "normal" ||
-      addon.type != "extension" ||
-      addon.id.endsWith("mozilla.org")
-    ) {
-      continue;
-    }
-
-    let rv = await fetch(`https://addons.thunderbird.net/api/v4/addons/addon/${addon.id}/versions/`);
-    let response = await rv.json();
-    //console.log("raw response", response);
-    addon["compatibility"] = "unknown";
-    addon["maxversion"] = "unknown";
-    let addonAltern = alternatives.filter(o => o.u_id === addon.id);
-    if (addonAltern.length) addon["alternative"] = "<a href = 'https://extension-finder.thunderbird.net/?id=" + addon.id + "' id = 'altaddon' >more info</a>";
-    else addon["alternative"] = "";
-    var comps = [], compVers = [];
-
-    if (
-      Array.isArray(response.results) &&
-      response.results.length > 0
-    ) {
-
-      for (let result of response.results) {
-        addon["compatibility"] = "no";
-        comps.push(result.compatibility.thunderbird.max);
-        let compat = result.compatibility.thunderbird.max;
-        let vers = {};
-        vers.TBvers = compat;
-        vers.version = result.version;
-
-        let urlStr = result.url;
-        vers.url = urlStr.substr(0, urlStr.indexOf("/version"));
-        //   console.log("add url", vers.url);
-
-        compVers.push(vers);
-
-        if (compat == "*" ||
-          parseInt(compat.split(".").shift()) >= 91) {
-          addon["compatibility"] = compat;
-
-        }
-
-
-
-      };
-      let compSort = comps.sort(compareVer);
-      //   console.log("ordered version", compSort);
-      //   console.log("ordered version", compVers);
-      let versSort = compVers.sort((a, b) => { compareVer(a.version, b.version) });
-      //   console.log(versSort);
-      addon["maxversion"] = versSort[0].version;
-      addon["url"] = versSort[0].url;
-
-      let data = response.results[0];
-      //    console.log(response.results[0].url);
-      // addon["compatibility"] = data.compatibility.thunderbird.max;
-      let info = {
-        name: addon.name,
-        version: data.version,
-        compatibility: data.compatibility.thunderbird.max,
-        alternative: "yes",
-        enabled: addon.enabled,
-      };
-    }
-
-
-    addons.push(addon);
-
-
-
-  };
-
-
-  bgrPage = await messenger.runtime.getBackgroundPage();
-  // if (!bgrPage.dataSet) console.log("onLoad", bgrPage);
-
-  var printBtn = document.getElementById("print");
-  printBtn.addEventListener("click", printAddons, true);
-  let table = document.getElementById("example1");
-  showTable();
+  // Enable debug mode on debug installs.
+  let self = await messenger.management.getSelf();
+  if (self.installType == "development") {
+      debug = true;
+  }
+  loadTable();
 }
-
-document.addEventListener("DOMContentLoaded", onLoad, false);
-
-
-
 
 const createdCell = function (cell, cellData, rowData, rowIndex, colIndex) {
-  let original;
-  // console.log(cell);
- 
-  if (colIndex == 2) {
-    //  console.log(cell);
-    if (cell.childNodes[0].data != "*") cell.bgColor = "#fc2c03";//"red";//
-    //cell.style = "{ color:red  !important}";
+  if ([1,2,3,4,5].includes(colIndex)) {
+    cell.style["text-align"] = "center";
+  }
+  if (!rowData.enabled) {
+    cell.style["color"] = "#888888"; // darker grey
+  }
+
+  if (colIndex == 4 && rowData.enabled) {
+    switch (rowData.status) {
+      case "isUnknown": 
+        cell.bgColor = "#A0A0A0"; // grey
+        break;
+      case "isIncompatible":
+        cell.bgColor = "#fc2c03"; //red
+        break;
+      case "hasAlternative":
+          cell.bgColor = "orange";
+          break;
+      case "isOk":
+          cell.bgColor = "green";
+          break;
+      }          
     cell.id = "td" + rowIndex + 'c' + colIndex;
-    currentCell = cell;
     cell.setAttribute('row', rowIndex);
     cell.setAttribute('column', colIndex);
   };
- 
+
+  if (colIndex == 5 && rowData.enabled && rowData.status == "hasAlternative") {  
+    cell.bgColor = "orange";
+  }
 }
 
+async function loadTable() {
+  let { addonData } = await messenger.storage.local.get("addonData");
+  let addons = Object.values(addonData);
+  log({addons});
+ 
+  document.getElementById("otherAddons").addEventListener("click", showOptoAddons, true);
+  document.getElementById("print").addEventListener("click", printAddons, true);
 
-
-var table = null;
-
-
-
-async function showTable() {
-
-
-  setTimeout(function () {
-    let z = 0;;
-  }, 200);
-
-  //console.log("showtable");
+	let { lastCheck } = await messenger.storage.local.get("lastCheck");
+  let date = new Date(lastCheck);
+  document.getElementById("lastUpdate").textContent = date.toString();
 
   $(document).ready(function () {
-
-    table = $('#example1').DataTable(
-      {
-        "pageLength": 25,
-        data: addons,
-        "order": [[1, "desc"]],
-        "dom": 'rtip',
-
-        columnDefs: [{
-          targets: '_all',
-          createdCell: createdCell
-        }],
-
-        columns: [
-          { title: "Addon name", width: "20%", data: "name" },
-          { title: "Active", width: "7%", data: "enabled" },
-          { title: "TB 91 compatible", width: "10%", data: "compatibility" },
-          { title: "Alternative", width: "10%", data: "alternative" },
-          { title: "Installed version", data: "version" },
-          { title: "Highest version on ATN", data: "maxversion" },
-          {
-            title: "Description",
-            //                width: "8%",
-            data: "description",
-          }
-        ]
-
-      }
-    );
-
+    $('#example1').DataTable({
+      "pageLength": 25,
+      destroy: true,
+      data: addons,
+      "order": [[1, "desc"]],
+      "dom": 'rtip',
+  
+      columnDefs: [{
+        targets: '_all',
+        createdCell: createdCell
+      }],
+  
+      columns: [
+        { title: "Addon name", width: "20%", data: "name" },
+        { title: "Active", width: "7%", data: "enabled" },
+        { title: "Installed version", data: "version", align: "center" },
+        { title: "Highest version on ATN", data: "maxUrl" },
+        { title: "Compatibility", width: "10%", data: "maxCompatibility", align: "center" },
+        { title: "Alternative", width: "10%", data: "alternative" },
+        { title: "Description", data: "description" }
+      ]
+    });
     let lod = document.getElementById("loading");
     lod.hidden = true;
-
-
-    // console.log (table);
-    // console.log(table.data().toArray());
-
-
   });
-
-
 }
 
+async function runtimeListener(request, sender) {
+	switch (request.command) {
+		case "reloadView":
+      loadTable();
+      break;
+	}
+}
 
-
-//);
-
-
+messenger.runtime.onMessage.addListener(runtimeListener);
+document.addEventListener("DOMContentLoaded", onLoad, false);
